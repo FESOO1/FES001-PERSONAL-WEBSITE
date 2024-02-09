1606516ab0a1a762af6014b81ace5da5689b86ce
FES001 <firdavs7794948@gmail.com>
2024-02-10 00:42:11 +0500

    s

--- script.js
+++ script.js
@@ -4,27 +4,32 @@
 
 const topBtn = document.querySelector('.navbar-change-top-link');
 const bottomBtn = document.querySelector('.navbar-change-bottom-link');
-const navbarContainer = document.querySelector('.navbar');
+const navMenu = document.querySelector('.navbar');
 
 function bottomPosition() {
     bottomBtn.style.display = 'none';
 
-    if(bottomBtn.style.display = 'none') {
+    if(bottomBtn.style.display == 'none') {
         topBtn.style.display = 'unset'
         return
     }
 
-    if(bottomBtn.style.display = 'none') {
+    if(bottomBtn.style.display == 'none') {
         navbarContainer.style.bottom = '5px';
         return
     }
+
+    navMenu.style.bottom = '5px';
 }
 
 function topPosition() {
     topBtn.style.display = 'none';
 
-    if(topBtn.style.display = 'none') {
+    if(topBtn.style.display == 'none') {
         bottomBtn.style.display = 'unset'
         return
     }
+    
+
+    navMenu.style.top = '5px';
 }
