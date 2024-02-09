5734a909cd81b39e29a1f46880beb1e6e82dc67e
FES001 <firdavs7794948@gmail.com>
2024-02-09 23:47:00 +0500

    s

---
+++ script.js
@@ -0,0 +1,23 @@
+// NAVBAR
+
+/* POSITION NAVBAR */
+
+const topBtn = document.querySelector('.position-top');
+const bottomBtn = document.querySelector('.position-bottom');
+
+function bottomPosition() {
+    bottomBtn.style.display = 'none';
+
+    if(bottomBtn.style.display = 'none') {
+        topBtn.style.display = 'unset'
+        return
+    }
+}
+
+function topPosition() {
+    topBtn.style.display = 'none';
+
+    if(topBtn.style.display = 'none') {
+        bottomBtn.style.display = 'unset'
+    }
+}
