f2cf088c884cf4d2a950ede3f22a3fa7fa163336
FES001 <firdavs7794948@gmail.com>
2024-02-10 14:08:16 +0500

    s

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 
 /* POSITION NAVBAR */
 
+const bodyofthepage = document.querySelector('body');
 
+document.querySelector('.navbar-changer').addEventListener('click', () =>{
+    bodyofthepage.classList.toggle('bodysssdadajs');
+})
