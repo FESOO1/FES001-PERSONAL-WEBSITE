666b3748c0c0b08d33e7b5a0fc0c0a110e5d1292
FES001 <firdavs7794948@gmail.com>
2024-02-10 14:05:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,4 @@
 
 /* POSITION NAVBAR */
 
-const svgSelect = document.querySelector('.position-svg');
 
-document.querySelector('.navbar-change-link').addEventListener('click', () =>{
-    svgSelect.classList.toggle('svgjs');
-})
-
-
-const navbarSelect = document.querySelector('.navbar');
-
-document.querySelector('.navbar-changer').addEventListener('click', () => {
-    navbarSelect.classList.toggle('navbarjs')
-})
