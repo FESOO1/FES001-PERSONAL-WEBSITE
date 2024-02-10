f700311d74f8a4da2c406eb78f5e9ebb2c1f7683
FES001 <firdavs7794948@gmail.com>
2024-02-10 14:04:49 +0500

    s

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ const svgSelect = document.querySelector('.position-svg');
 document.querySelector('.navbar-change-link').addEventListener('click', () =>{
     svgSelect.classList.toggle('svgjs');
 })
+
+
+const navbarSelect = document.querySelector('.navbar');
+
+document.querySelector('.navbar-changer').addEventListener('click', () => {
+    navbarSelect.classList.toggle('navbarjs')
+})
