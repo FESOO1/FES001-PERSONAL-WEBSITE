2b4c7b7c3b38da7b2127467859094cdf3895c085
FES001 <firdavs7794948@gmail.com>
2024-02-10 16:36:32 +0500

    s

--- script.js
+++ script.js
@@ -2,26 +2,6 @@
 
 /* POSITION NAVBAR */
 
-/* let bottomSvg = document.querySelector('.navbar-change-link');
-let topSvg = document.querySelector('.navbar-change-link1');
-let navbarItself = document.querySelector('.navbar');
-
-function bottomPosition() {
-    bottomSvg.style.display = 'none'
-    if(bottomSvg.style.display = 'none') {
-        topSvg.style.display = 'unset'
-        return
-    }
-}
-
-function topPosition() {
-    topSvg.style.display = 'none'
-    if(topSvg.style.display = 'none') {
-        bottomSvg.style.display = 'unset'
-        return
-    }
-} */
-
 const themeChanger = document.querySelector('.theme-changer-link');
 const themeSvg = document.querySelector('.position-svg');
 
